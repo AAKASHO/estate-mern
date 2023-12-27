bb3866b36a9df66e48487c4c087ea3b078a0824d
akash kumar <iamaakashkumar789@gmail.com>
2023-12-28 02:40:22 +0530

    complete upload image of listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,7 +31,7 @@ export const updateUser= async(req,res,next)=>{
         }
     }
 export const deleteUser=async(req,res,next)=>{
-    if(req.user.id!=req.params.id) return next(errorHandler(401,'you can nly delete your own account!'))
+    if(req.user.id!=req.params.id) return next(errorHandler(401,'you can only delete your own account!'))
 
     try{
         await User.findByIdAndDelete(req.params.id);
